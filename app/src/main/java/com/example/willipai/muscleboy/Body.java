dcf3540e515aeaabab56de826c051322d36d55d5
William Kang <Willipai@williams-mbp.dhcp.wustl.edu>
2017-10-28 16:17:00 -0400

    no change

--- app/src/main/java/com/example/willipai/muscleboy/Body.java
+++ app/src/main/java/com/example/willipai/muscleboy/Body.java
@@ -1,13 +1,20 @@
 package com.example.willipai.muscleboy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
-public class Body extends AppCompatActivity {
+import static android.graphics.Color.WHITE;
+
+public class Body extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
+        ImageView abs = (ImageView)findViewById(R.id.imageView55);
+        abs.setColorFilter(WHITE, PorterDuff.Mode.MULTIPLY);
     }
 }
--- app/src/main/java/com/example/willipai/muscleboy/MainActivity.java
+++ app/src/main/java/com/example/willipai/muscleboy/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.willipai.muscleboy;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -14,7 +13,7 @@ public class MainActivity extends Activity {
     public ImageButton bodyBut;
 
         public void init(){
-            bodyBut = findViewById(R.id.button1);
+            bodyBut = findViewById(R.id.workout);
             bodyBut.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
