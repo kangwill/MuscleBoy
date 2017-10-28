5877eba398798e4e3541c3740a4aaeeeabfa2789
William Kang <Willipai@williams-mbp.dhcp.wustl.edu>
2017-10-28 11:09:39 -0400

    Created body

---
+++ app/src/main/java/com/example/willipai/muscleboy/Body.java
@@ -0,0 +1,13 @@
+package com.example.willipai.muscleboy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+/**
+ * Created by Willipai on 10/28/17.
+ */
+
+public class Body extends Activity {
+
+}
--- app/src/main/java/com/example/willipai/muscleboy/MainActivity.java
+++ app/src/main/java/com/example/willipai/muscleboy/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.willipai.muscleboy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +15,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        Button next = findViewById(R.id.imageButton7);
+    }
+
+    public void move_to_body(View view){
+        Intent intent = new Intent(this, )
     }
 }
