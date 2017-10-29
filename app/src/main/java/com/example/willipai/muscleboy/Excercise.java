5f580c6da4c9e58bf29d2fe2a6897d5f4e4ce2c9
kalpanjasani <kjasani@purdue.edu>
2017-10-28 19:45:41 -0500

    Kalpan added features

---
+++ app/src/main/java/com/example/willipai/muscleboy/Excercise.java
@@ -0,0 +1,10 @@
+package com.example.willipai.muscleboy;
+
+/**
+ * Created by kalpanjasani on 10/28/17.
+ */
+
+public class Excercise {
+    String name;
+    String[] muscleGroups;
+}
--- app/src/main/java/com/example/willipai/muscleboy/MainActivity.java
+++ app/src/main/java/com/example/willipai/muscleboy/MainActivity.java
@@ -5,23 +5,41 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 import android.widget.ImageButton;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
+    /*buttons*/
     public ImageButton bodyBut;
-
-        public void init(){
-            bodyBut = (ImageButton)findViewById(R.id.imageButton5);
-            bodyBut.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent first = new Intent(MainActivity.this, Body.class);
-                    startActivity(first);
-                }
+    public ImageButton journalButton;
+    public ImageButton questionButton;
+    public ImageButton settingsButton;
+    public void init(){
+        bodyBut = (ImageButton)findViewById(R.id.imageButton5);
+        bodyBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent first = new Intent(MainActivity.this, Body.class);
+                startActivity(first);
+            }
+        });
+        journalButton = findViewById(R.id.imageButton4);
+        journalButton.setOnClickListener(new View.OnClickListener()
+            {
+               public void onClick(View v)
+               {
+                   Intent journalIntent = new Intent(MainActivity.this, Journal.class);
+                   startActivity(journalIntent);
+               }
             });
-        }
+
+
+
+    }
+
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
