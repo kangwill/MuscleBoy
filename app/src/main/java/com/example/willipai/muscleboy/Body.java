382f7dd87ed78d367d47dddb9e5c336c4b48309d
William Kang <Willipai@williams-mbp.dhcp.wustl.edu>
2017-10-28 20:26:26 -0400

    white body

--- app/src/main/java/com/example/willipai/muscleboy/Body.java
+++ app/src/main/java/com/example/willipai/muscleboy/Body.java
@@ -1,16 +1,33 @@
 package com.example.willipai.muscleboy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Body extends Activity{
+
+    public Button backView;
+
+    public void init() {
+        backView = (Button) findViewById(R.id.button3);
+        backView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent first = new Intent(Body.this, BackBody.class);
+                startActivity(first);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_body);
-
+        init();
     }
 }
